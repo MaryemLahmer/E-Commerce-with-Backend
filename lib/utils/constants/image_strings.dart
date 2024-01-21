1701e309f0bf61e367a8baa8841fbd467a773456
MaryemLahmer <maryem.lahmer@ensi-uma.tn>
2024-01-21 20:58:56 +0100

    Update

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,14 @@
+///This class contains all the App images in string formats
+
+class MImages{
+
+  //-- App logos
+  static const String darkAppLogo="assets/logos/splash-logo-dark.png";
+  static const String lightAppLogo="assets/logos/splash-logo-light.png";
+
+  //-- Social logos
+  static const String google="assets/logos/google-icon.png";
+  static const String facebook="assets/logos/facebook-icon.png";
+
+
+}
