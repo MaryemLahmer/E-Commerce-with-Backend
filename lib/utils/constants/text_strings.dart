9f64b22f463a969e070795a4cd893bafe469eb0a
MaryemLahmer <maryem.lahmer@ensi-uma.tn>
2024-01-27 13:00:02 +0100

    updating the text strings

--- lib/utils/constants/text_strings.dart
+++ lib/utils/constants/text_strings.dart
@@ -4,6 +4,9 @@
 class MTexts{
 
   //-- Global Texts
+  static const String submit = "Submit";
+  static const String appName = "M-Store";
+  static const String tContinue = "Continue";
 
   //-- Onboarding Texts
   static const String onBoardingTitle1 = "Choose your product";
@@ -18,6 +21,38 @@ class MTexts{
   static const String homeAppBarTitle = "Good day for Shopping";
   static const String homeAppBarSubTitle = "Marie Antoinette";
 
+  // -- Authentication Form Text
+  static const String firstName="First Name";
+  static const String lastName ="Last Name";
+  static const String email ="E-mail";
+  static const String password="Password";
+  static const String newPassword="New Password";
+  static const String username ="Username";
+  static const String phoneNo="Phone Number";
+  static const String forgotPassword="Forgot Password ?";
+  static const String signIn ="Sign In";
+  static const String createAccount="Create Account";
+  static const String orSignInWith="Or Sign In With";
+  static const String iAgreeTo="I Agree To";
+  static const String privacyPolicy="Privacy policy";
+  static const String termsOfUse="Terms Of Use";
+  static const String verificationCode  ="Verification Code";
+  static const String resendEmail ="Resend E-mail";
+  static const String resendEmailIn="Resend E-mail In";
+
+  //-- Authentication Headings Text
+  static const String loginTitle ="Welcome Back";
+  static const String loginSubtitle="Discover Limitless Choices and Unmatched Conveniences";
+  static const String signupTitle="Let's Create your Account";
+  static const String forgotPasswordTitle="Forgot Password";
+  static const String forgotPasswordSubtitle ="Enter Your E-mail and We Will Send You a Password Reset Link";
+  static const String changePasswordTitle ="Password Reset E-mail Sent";
+  static const String changePasswordSubtitle="Your Account Security is Our Priority! We've Sent You a Secure Link to Safely Change your Password and Keep Your Account Protected ";
+  static const String confirmEmail="Verify your E-mail Address";
+  static const String confirmEmailSubtitle="Congratulations! Your account awaits: Verify your E-mail to Start Shopping and Experience a World of Unrivaled Deals and Personalised Offers";
+  static const String emailNotReceivedMessage="Didn't get the E-mail ? Check your Spam or resend it";
+  static const String yourAccountCreatedTitle="Your Account is Successfully Created";
+  static const String yourEmailCreatedSubtitle="Welcome to your Ultimate Shopping Destination: Your Account is Created, Unleash the Joy of Seamless Online Shopping";
 
 
 
