79810807577aff49cf034cf8c46d12bc74605d1a
MaryemLahmer <maryem.lahmer@ensi-uma.tn>
2024-01-28 00:17:16 +0100

    create login form in a separate class

---
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import '../../../../../utils/constants/sizes.dart';
+import '../../../../../utils/constants/text_strings.dart';
+import '../../sign_up/widgets/signup.dart';
+
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+              vertical: MSizes.spaceBetweenSections),
+          child: Column(
+            children: [
+              ///Email
+              TextFormField(
+                decoration: const InputDecoration(
+                    prefixIcon: Icon(Iconsax.direct_right),
+                    labelText: MTexts.email),
+              ),
+              const SizedBox(height: MSizes.spaceBtwInputFields),
+
+              //Password
+              TextFormField(
+                decoration: const InputDecoration(
+                    prefixIcon: Icon(Iconsax.password_check),
+                    labelText: MTexts.password,
+                    suffixIcon: Icon(Iconsax.eye_slash)),
+              ),
+              const SizedBox(height: MSizes.spaceBtwInputFields / 2),
+
+              /// Remember Me and Forget Password
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  /// Remember Me
+                  Row(
+                    children: [
+                      Checkbox(value: true, onChanged: (value) {}),
+                      const Text(MTexts.rememberMe),
+                    ],
+                  ),
+
+                  /// Forgot Password
+                  TextButton(
+                      onPressed: () {},
+                      child: const Text(MTexts.forgotPassword)),
+                ],
+              ),
+              const SizedBox(
+                height: MSizes.spaceBetweenSections,
+              ),
+
+              /// Sign in button
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                    onPressed: () {}, child: const Text(MTexts.signIn)),
+              ),
+              const SizedBox(
+                height: MSizes.spaceBetweenItems,
+              ),
+
+              /// Create Account Button
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                    onPressed: () => Get.to(() => const SignUpScreen(),),
+                    child: const Text(MTexts.createAccount)),
+              ),
+            ],
+          ),
+        ));
+  }
+}
