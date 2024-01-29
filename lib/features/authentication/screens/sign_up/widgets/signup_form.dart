9512d9ae3cafa88a49dcd65371ac122c7bc1d9c2
MaryemLahmer <maryem.lahmer@ensi-uma.tn>
2024-01-30 00:02:59 +0100

    updating sign up form

--- lib/features/authentication/screens/sign_up/widgets/signup_form.dart
+++ lib/features/authentication/screens/sign_up/widgets/signup_form.dart
@@ -1,8 +1,11 @@
 import 'package:e_commerce_with_backend/features/authentication/screens/sign_up/widgets/terms_conditions_checkbox.dart';
+import 'package:e_commerce_with_backend/features/authentication/screens/sign_up/widgets/verify_email.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 import '../../../../../utils/constants/sizes.dart';
 import '../../../../../utils/constants/text_strings.dart';
+
 class SignupForm extends StatelessWidget {
   const SignupForm({
     super.key,
@@ -10,94 +13,88 @@ class SignupForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Form(
         child: Column(
+      children: [
+        Row(
           children: [
-            Row(
-              children: [
-                Expanded(
-                  child: TextFormField(
-                    expands: false,
-                    decoration: const InputDecoration(
-                        labelText: MTexts.firstName,
-                        prefixIcon: Icon(Iconsax.user)),
-                  ),
-                ),
-                const SizedBox(
-                  width: MSizes.spaceBtwInputFields,
-                ),
-                Expanded(
-                  child: TextFormField(
-                    expands: false,
-                    decoration: const InputDecoration(
-                        labelText: MTexts.lastName,
-                        prefixIcon: Icon(Iconsax.user)),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: MSizes.spaceBtwInputFields,
-            ),
-
-            /// Username
-            TextFormField(
-              decoration: const InputDecoration(
-                  labelText: MTexts.username,
-                  prefixIcon: Icon(Iconsax.user_edit)),
-            ),
-            const SizedBox(
-              height: MSizes.spaceBtwInputFields,
-            ),
-
-            /// Email
-            TextFormField(
-              decoration: const InputDecoration(
-                  labelText: MTexts.email,
-                  prefixIcon: Icon(Iconsax.direct)),
-            ),
-            const SizedBox(
-              height: MSizes.spaceBtwInputFields,
-            ),
-
-            /// Phone Number
-            TextFormField(
-              decoration: const InputDecoration(
-                  labelText: MTexts.phoneNo,
-                  prefixIcon: Icon(Iconsax.call)),
-            ),
-            const SizedBox(
-              height: MSizes.spaceBtwInputFields,
-            ),
-
-            ///Password
-            TextFormField(
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: MTexts.password,
-                prefixIcon: Icon(Iconsax.password_check),
-                suffixIcon: Icon(Iconsax.eye_slash),
+            Expanded(
+              child: TextFormField(
+                expands: false,
+                decoration: const InputDecoration(
+                    labelText: MTexts.firstName,
+                    prefixIcon: Icon(Iconsax.user)),
               ),
             ),
             const SizedBox(
-              height: MSizes.spaceBetweenSections,
+              width: MSizes.spaceBtwInputFields,
             ),
-
-            /// Terms and Conditions
-            const TermsAndConditionsCheckBox(),
-            const SizedBox(
-              height: MSizes.spaceBetweenSections,
+            Expanded(
+              child: TextFormField(
+                expands: false,
+                decoration: const InputDecoration(
+                    labelText: MTexts.lastName, prefixIcon: Icon(Iconsax.user)),
+              ),
             ),
-
-            /// Sign Up Button
-            SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                    onPressed: () {},
-                    child: const Text(MTexts.createAccount)))
           ],
-        ));
+        ),
+        const SizedBox(
+          height: MSizes.spaceBtwInputFields,
+        ),
+
+        /// Username
+        TextFormField(
+          decoration: const InputDecoration(
+              labelText: MTexts.username, prefixIcon: Icon(Iconsax.user_edit)),
+        ),
+        const SizedBox(
+          height: MSizes.spaceBtwInputFields,
+        ),
+
+        /// Email
+        TextFormField(
+          decoration: const InputDecoration(
+              labelText: MTexts.email, prefixIcon: Icon(Iconsax.direct)),
+        ),
+        const SizedBox(
+          height: MSizes.spaceBtwInputFields,
+        ),
+
+        /// Phone Number
+        TextFormField(
+          decoration: const InputDecoration(
+              labelText: MTexts.phoneNo, prefixIcon: Icon(Iconsax.call)),
+        ),
+        const SizedBox(
+          height: MSizes.spaceBtwInputFields,
+        ),
+
+        ///Password
+        TextFormField(
+          obscureText: true,
+          decoration: const InputDecoration(
+            labelText: MTexts.password,
+            prefixIcon: Icon(Iconsax.password_check),
+            suffixIcon: Icon(Iconsax.eye_slash),
+          ),
+        ),
+        const SizedBox(
+          height: MSizes.spaceBetweenSections,
+        ),
+
+        /// Terms and Conditions
+        const TermsAndConditionsCheckBox(),
+        const SizedBox(
+          height: MSizes.spaceBetweenSections,
+        ),
+
+        /// Sign Up Button
+        SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+                onPressed: () => Get.to(() => const VerifyEmailScreen()),
+                child: const Text(MTexts.createAccount)))
+      ],
+    ));
   }
 }
-
