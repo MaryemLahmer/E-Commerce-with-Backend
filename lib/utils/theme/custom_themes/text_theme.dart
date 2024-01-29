8d000f4098a1467953386c9a96a9df33664bc4f5
MaryemLahmer <maryem.lahmer@ensi-uma.tn>
2024-01-29 22:48:06 +0100

    updating themes

--- lib/utils/theme/custom_themes/text_theme.dart
+++ lib/utils/theme/custom_themes/text_theme.dart
@@ -14,7 +14,7 @@ class MTextTheme{
     titleSmall: const TextStyle().copyWith(fontSize: 16.0,fontWeight: FontWeight.w400,color: Colors.black),
 
     bodyLarge: const TextStyle().copyWith(fontSize: 14.0,fontWeight: FontWeight.w500,color: Colors.black),
-    bodyMedium: const TextStyle().copyWith(fontSize: 14.0,fontWeight: FontWeight.normal,color: Colors.black),
+    bodyMedium: const TextStyle().copyWith(fontSize: 12.0,fontWeight: FontWeight.w400,color: Colors.black),
     bodySmall: const TextStyle().copyWith(fontSize: 14.0,fontWeight: FontWeight.w500,color: Colors.black.withOpacity(0.5)),
 
     labelLarge:const TextStyle().copyWith(fontSize: 12.0,fontWeight: FontWeight.normal,color: Colors.black),
@@ -24,8 +24,8 @@ class MTextTheme{
 
   /// Customizable dark theme text
   static TextTheme darkTextTheme = TextTheme(
-    headlineLarge: const TextStyle().copyWith(fontSize: 32.0,fontWeight: FontWeight.bold,color: Colors.white),
-    headlineMedium: const TextStyle().copyWith(fontSize: 24.0,fontWeight: FontWeight.w600,color: Colors.white),
+    headlineLarge: const TextStyle().copyWith(fontSize: 34.0,fontWeight: FontWeight.bold,color: Colors.white),
+    headlineMedium: const TextStyle().copyWith(fontSize: 22.0,fontWeight: FontWeight.w400, color: Colors.white),
     headlineSmall: const TextStyle().copyWith(fontSize: 18.0,fontWeight: FontWeight.w600,color: Colors.white),
 
     titleLarge: const TextStyle().copyWith(fontSize: 16.0,fontWeight: FontWeight.w600,color: Colors.white),
