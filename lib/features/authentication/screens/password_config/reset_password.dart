4d46e101df99528572ec17ebe4c6b56a6f131709
MaryemLahmer <maryem.lahmer@ensi-uma.tn>
2024-01-31 23:24:15 +0100

    creating reset password screen

--- lib/features/authentication/screens/password_config/reset_password.dart
+++ lib/features/authentication/screens/password_config/reset_password.dart
@@ -1,31 +1,65 @@
+import 'package:e_commerce_with_backend/utils/constants/image_strings.dart';
 import 'package:e_commerce_with_backend/utils/constants/sizes.dart';
 import 'package:e_commerce_with_backend/utils/constants/text_strings.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class ForgotPassword extends StatelessWidget {
-  const ForgotPassword({super.key});
+import '../../../../utils/helpers/helper_functions.dart';
+
+class ResetPassword extends StatelessWidget {
+  const ResetPassword({super.key,});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body:  Padding(
-        padding: const EdgeInsets.all(MSizes.defaultSpace),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(onPressed: ()=> Get.back(), icon: const Icon(CupertinoIcons.clear))
+        ],
+      ),
+      body:  SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(MSizes.defaultSpace),
+          child: Column(
           children: [
-            /// Headings
-            Text(MTexts.forgotPasswordTitle, style: Theme.of(context).textTheme.headlineMedium,),
-            const SizedBox(height: MSizes.spaceBetweenItems,),
-            Text(MTexts.forgotPasswordSubtitle, style: Theme.of(context).textTheme.labelMedium,),
-            const SizedBox(height: MSizes.spaceBetweenSections * 2,),
-
-
-            /// Text Field
-
-            /// Submit Button
-
+            
+            /// Image with 60% of screen width
+            Image(
+                image: const AssetImage(MImages.deliveredEmailIllustration),
+                width: MHelperFunctions.screenWidth() * 0.6),
+            const SizedBox(
+              height: MSizes.spaceBetweenSections,
+            ),
+            /// Title and Subtitle
+            Text(
+              MTexts.changePasswordTitle,
+              style: Theme.of(context).textTheme.headlineMedium,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: MSizes.spaceBetweenItems,
+            ),
+            const SizedBox(
+              height: MSizes.spaceBetweenItems,
+            ),
+            Text(
+              MTexts.changePasswordSubtitle,
+              style: Theme.of(context).textTheme.labelMedium,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: MSizes.spaceBetweenSections,
+            ),
+            /// Buttons
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: (){}, child: const Text(MTexts.done)),
+            ),
           ],
+          ),
         ),
       ),
     );
