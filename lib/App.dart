40f6cab594ab704fc4df009d9ea7b375ea6a2d2d
MaryemLahmer <maryem.lahmer@ensi-uma.tn>
2024-01-31 22:49:35 +0100

    update app.dart

--- lib/App.dart
+++ lib/App.dart
@@ -2,7 +2,6 @@ import 'package:e_commerce_with_backend/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'features/authentication/screens/onboarding_screens/onboarding.dart';
-import 'features/authentication/screens/sign_up/widgets/signup.dart';
 
 //--Use this Class to setup themes, initial Bindings, any animations and much more
 class App extends StatelessWidget {
@@ -14,8 +13,7 @@ class App extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: MAppTheme.lightTheme,
       darkTheme: MAppTheme.darkTheme,
-      //home: const OnBoardingScreen(),
-      home: const SignUpScreen(),
+      home: const OnBoardingScreen(),
     );
   }
 }
