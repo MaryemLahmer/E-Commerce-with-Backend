29da4a1eddf537266781556b2b107abf119044f0
MaryemLahmer <maryem.lahmer@ensi-uma.tn>
2024-01-31 23:24:30 +0100

    updating forgot password screen

--- lib/features/authentication/screens/password_config/forget_password.dart
+++ lib/features/authentication/screens/password_config/forget_password.dart
@@ -10,26 +10,45 @@ class ForgotPassword extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body:  Padding(
+      body: Padding(
         padding: const EdgeInsets.all(MSizes.defaultSpace),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             /// Headings
-            Text(MTexts.forgotPasswordTitle, style: Theme.of(context).textTheme.headlineMedium,),
-            const SizedBox(height: MSizes.spaceBetweenItems,),
-            Text(MTexts.forgotPasswordSubtitle, style: Theme.of(context).textTheme.labelMedium,),
-            const SizedBox(height: MSizes.spaceBetweenSections * 2,),
+            Text(
+              MTexts.forgotPasswordTitle,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(
+              height: MSizes.spaceBetweenItems,
+            ),
+            Text(
+              MTexts.forgotPasswordSubtitle,
+              style: Theme.of(context).textTheme.labelMedium,
+            ),
+            const SizedBox(
+              height: MSizes.spaceBetweenSections * 2,
+            ),
 
             /// Text Field
             TextFormField(
               decoration: const InputDecoration(
-                labelText: MTexts.email,
-                prefixIcon: Icon(Iconsax.direct_right)
+                  labelText: MTexts.email,
+                  prefixIcon: Icon(Iconsax.direct_right)),
+            ),
+            const SizedBox(
+              height: MSizes.spaceBetweenSections,
+            ),
+
+            /// Submit Button
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () => Get.off(() => ResetPassword()),
+                child: const Text(MTexts.submit),
               ),
             )
-            /// Submit Button
-
           ],
         ),
       ),
