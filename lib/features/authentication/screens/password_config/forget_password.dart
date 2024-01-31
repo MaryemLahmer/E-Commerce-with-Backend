e9abb28faa4a986e8f34234e2f4fc0acf4a6e93c
MaryemLahmer <maryem.lahmer@ensi-uma.tn>
2024-01-31 23:04:37 +0100

    creating forgot password screen

---
+++ lib/features/authentication/screens/password_config/forget_password.dart
@@ -0,0 +1,38 @@
+import 'package:e_commerce_with_backend/utils/constants/sizes.dart';
+import 'package:e_commerce_with_backend/utils/constants/text_strings.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class ForgotPassword extends StatelessWidget {
+  const ForgotPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body:  Padding(
+        padding: const EdgeInsets.all(MSizes.defaultSpace),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            /// Headings
+            Text(MTexts.forgotPasswordTitle, style: Theme.of(context).textTheme.headlineMedium,),
+            const SizedBox(height: MSizes.spaceBetweenItems,),
+            Text(MTexts.forgotPasswordSubtitle, style: Theme.of(context).textTheme.labelMedium,),
+            const SizedBox(height: MSizes.spaceBetweenSections * 2,),
+
+            /// Text Field
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: MTexts.email,
+                prefixIcon: Icon(Iconsax.direct_right)
+              ),
+            )
+            /// Submit Button
+
+          ],
+        ),
+      ),
+    );
+  }
+}
