be147fa0c2b7b9bac3617463491a251b44366ccc
MaryemLahmer <maryem.lahmer@ensi-uma.tn>
2024-02-02 23:12:13 +0100

    separating home app bar widget

---
+++ lib/features/shop/screens/home/widgets/home_appbar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../common/widgets/appbar/appbar.dart';
+import '../../../../../common/widgets/products/cart/cart_menu_item.dart';
+import '../../../../../utils/constants/colors.dart';
+import '../../../../../utils/constants/text_strings.dart';
+
+class HomeAppBar extends StatelessWidget {
+  const HomeAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MAppBar(
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            MTexts.homeAppBarTitle,
+            style: Theme.of(context)
+                .textTheme
+                .labelMedium!
+                .apply(color: MColors.grey),
+          ),
+          Text(
+            MTexts.homeAppBarSubTitle,
+            style: Theme.of(context)
+                .textTheme
+                .headlineSmall!
+                .apply(color: MColors.white),
+          )
+        ],
+      ),
+      actions: [
+        CartCounterItem(
+          onPressed: () {}, iconColor: MColors.white,
+        )
+      ],
+    );
+  }
+}
