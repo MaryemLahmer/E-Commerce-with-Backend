431dee98e69f9c911c0cf702c87fa8f7cbae1d98
MaryemLahmer <maryem.lahmer@ensi-uma.tn>
2024-02-02 21:18:57 +0100

    updating home screen

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,10 +1,45 @@
+import 'package:e_commerce_with_backend/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
+import '../../../../common/widgets/custom_shapes/containers/circular_container.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              color: MColors.primary,
+              padding: const EdgeInsets.all(0),
+              child: SizedBox(
+                height: 320,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: -150,
+                      right: -250,
+                      child: CircularContainer(
+                        backgroundColor: MColors.textWhite.withOpacity(0.1),
+                      ),
+                    ),
+                    Positioned(
+                      top: 100,
+                      right: -300,
+                      child: CircularContainer(
+                        backgroundColor: MColors.textWhite.withOpacity(0.1),
+                      ),
+                    ),
+                    ///Column()
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
