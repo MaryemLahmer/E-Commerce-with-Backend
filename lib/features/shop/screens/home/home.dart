5c6be0bc78c6095d14580b84e71041a6f60819d9
MaryemLahmer <maryem.lahmer@ensi-uma.tn>
2024-02-02 23:12:26 +0100

    updating home screen

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,77 +1,21 @@
 import 'package:e_commerce_with_backend/common/widgets/appbar/appbar.dart';
-import 'package:e_commerce_with_backend/utils/constants/text_strings.dart';
+import 'package:e_commerce_with_backend/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
-import '../../../../utils/constants/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             PrimaryHeaderContainer(
               child: Column(
                 children: [
-                  MAppBar(
-                    title: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          MTexts.homeAppBarTitle,
-                          style: Theme.of(context)
-                              .textTheme
-                              .labelMedium!
-                              .apply(color: MColors.grey),
-                        ),
-                        Text(
-                          MTexts.homeAppBarSubTitle,
-                          style: Theme.of(context)
-                              .textTheme
-                              .headlineSmall!
-                              .apply(color: MColors.white),
-                        )
-                      ],
-                    ),
-                    actions: [
-                      Stack(
-                        children: [
-                          IconButton(
-                              onPressed: () {},
-                              icon: const Icon(
-                                Iconsax.shopping_bag,
-                                color: MColors.white,
-                              )),
-                          Positioned(
-                            right: 0,
-                            child: Container(
-                              width: 18,
-                              height: 18,
-                              decoration: BoxDecoration(
-                                color: MColors.black.withOpacity(0.5),
-                                borderRadius: BorderRadius.circular(80),
-                              ),
-                              child: Center(
-                                child: Text(
-                                  '2',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .labelLarge!
-                                      .apply(
-                                          color: MColors.white,
-                                          fontSizeFactor: 0.8),
-                                ),
-                              ),
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
+                  HomeAppBar(),
                 ],
               ),
             )
@@ -81,3 +25,5 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+
