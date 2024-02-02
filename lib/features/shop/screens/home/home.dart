baaaa54ea8f4b4559b63aa8731c07818df5f7620
MaryemLahmer <maryem.lahmer@ensi-uma.tn>
2024-02-02 23:01:40 +0100

    creating the shopping bag with items counter for the app bar in home screen

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,6 +1,7 @@
 import 'package:e_commerce_with_backend/common/widgets/appbar/appbar.dart';
 import 'package:e_commerce_with_backend/utils/constants/text_strings.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 import '../../../../utils/constants/colors.dart';
 
@@ -18,6 +19,7 @@ class HomeScreen extends StatelessWidget {
                 children: [
                   MAppBar(
                     title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
                           MTexts.homeAppBarTitle,
@@ -35,6 +37,40 @@ class HomeScreen extends StatelessWidget {
                         )
                       ],
                     ),
+                    actions: [
+                      Stack(
+                        children: [
+                          IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Iconsax.shopping_bag,
+                                color: MColors.white,
+                              )),
+                          Positioned(
+                            right: 0,
+                            child: Container(
+                              width: 18,
+                              height: 18,
+                              decoration: BoxDecoration(
+                                color: MColors.black.withOpacity(0.5),
+                                borderRadius: BorderRadius.circular(80),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  '2',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .labelLarge!
+                                      .apply(
+                                          color: MColors.white,
+                                          fontSizeFactor: 0.8),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
                   ),
                 ],
               ),
