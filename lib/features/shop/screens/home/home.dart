4e2c2cb9771fb2d5e09422756c30b4bb256b487b
MaryemLahmer <maryem.lahmer@ensi-uma.tn>
2024-02-02 22:53:13 +0100

    updating home screen with the appbar

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,19 +1,41 @@
+import 'package:e_commerce_with_backend/common/widgets/appbar/appbar.dart';
+import 'package:e_commerce_with_backend/utils/constants/text_strings.dart';
 import 'package:flutter/material.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
+import '../../../../utils/constants/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             PrimaryHeaderContainer(
               child: Column(
                 children: [
-
+                  MAppBar(
+                    title: Column(
+                      children: [
+                        Text(
+                          MTexts.homeAppBarTitle,
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelMedium!
+                              .apply(color: MColors.grey),
+                        ),
+                        Text(
+                          MTexts.homeAppBarSubTitle,
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineSmall!
+                              .apply(color: MColors.white),
+                        )
+                      ],
+                    ),
+                  ),
                 ],
               ),
             )
@@ -23,4 +45,3 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
-
