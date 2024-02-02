327d5e9812061b19ae678ee02d9a8af56ff8a7b2
MaryemLahmer <maryem.lahmer@ensi-uma.tn>
2024-02-02 20:07:10 +0100

    updating navigation menu with the home screen

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -1,3 +1,6 @@
+import 'package:e_commerce_with_backend/features/shop/screens/home/home.dart';
+import 'package:e_commerce_with_backend/utils/constants/colors.dart';
+import 'package:e_commerce_with_backend/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
@@ -8,6 +11,7 @@ class NavigationMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(NavigationController());
+    final isDark = MHelperFunctions.isDarkMode(context);
     return Scaffold(
       bottomNavigationBar: Obx(
         () => NavigationBar(
@@ -16,6 +20,8 @@ class NavigationMenu extends StatelessWidget {
           selectedIndex: controller.selectedIndex.value,
           onDestinationSelected: (index) =>
               controller.selectedIndex.value = index,
+          backgroundColor: isDark ? MColors.black : MColors.white,
+          indicatorColor: isDark ? MColors.white.withOpacity(0.1):MColors.black.withOpacity(0.1),
           destinations: const [
             NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
             NavigationDestination(icon: Icon(Iconsax.shop), label: 'Store'),
@@ -32,9 +38,7 @@ class NavigationMenu extends StatelessWidget {
 class NavigationController extends GetxController {
   final Rx<int> selectedIndex = 0.obs;
   final screens = [
-    Container(
-      color: Colors.red,
-    ),
+    const HomeScreen(),
     Container(
       color: Colors.purple,
     ),
