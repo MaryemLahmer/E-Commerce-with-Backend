310bf6d523ce39010009526fbc2d4a79ecdcc15a
MaryemLahmer <maryem.lahmer@ensi-uma.tn>
2024-02-03 23:30:51 +0100

    creating the search bar within the header

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,20 +1,38 @@
 import 'package:e_commerce_with_backend/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:e_commerce_with_backend/utils/constants/sizes.dart';
+import 'package:e_commerce_with_backend/utils/device/device_utility.dart';
 import 'package:flutter/material.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
+import '../../../../utils/constants/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             PrimaryHeaderContainer(
               child: Column(
                 children: [
-                  HomeAppBar(),
+                  /// -- AppBar
+                  const HomeAppBar(),
+
+                  /// -- SearchBar
+                  Container(
+                    width: MDeviceUtils.getScreenWidth(context),
+                    padding: const EdgeInsets.all(MSizes.md),
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(MSizes.cardRadiusLg),
+                      border: Border.all(color: MColors.grey)
+                    ),
+                  )
+
+
+                  /// -- Categories
                 ],
               ),
             )
