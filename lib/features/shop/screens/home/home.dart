b14b851abed2f648b8c2f6869c7eb75be0497488
MaryemLahmer <maryem.lahmer@ensi-uma.tn>
2024-02-04 23:14:59 +0100

    updating home screen with slider

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,10 +1,12 @@
 import 'package:e_commerce_with_backend/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:e_commerce_with_backend/features/shop/screens/home/widgets/home_categories.dart';
+import 'package:e_commerce_with_backend/features/shop/screens/home/widgets/promo_slider.dart';
 import 'package:e_commerce_with_backend/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 import '../../../../common/widgets/custom_shapes/containers/search_container.dart';
 import '../../../../common/widgets/texts/section_heading.dart';
+import '../../../../utils/constants/image_strings.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -48,12 +50,25 @@ class HomeScreen extends StatelessWidget {
                   HomeCategories()
                 ],
               ),
-            )
+            ),
+
+            /// Body
+            Padding(
+                padding: EdgeInsets.all(MSizes.defaultSpace),
+                child: PromoSlider(
+                  banners: [
+                    MImages.promoBanner1,
+                    MImages.promoBanner2,
+                    MImages.promoBanner3,
+                    MImages.promoBanner4,
+                    MImages.promoBanner5,
+                    MImages.promoBanner6,
+                    MImages.promoBanner7,
+                  ],
+                ))
           ],
         ),
       ),
     );
   }
 }
-
-
