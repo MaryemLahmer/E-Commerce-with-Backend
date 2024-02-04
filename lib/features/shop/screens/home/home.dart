e0f1a732f7596872d80383db27501fa6d0b8370a
MaryemLahmer <maryem.lahmer@ensi-uma.tn>
2024-02-04 03:07:48 +0100

    updating home screen with the category section

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,48 +1,57 @@
 import 'package:e_commerce_with_backend/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:e_commerce_with_backend/features/shop/screens/home/widgets/home_categories.dart';
 import 'package:e_commerce_with_backend/utils/constants/sizes.dart';
-import 'package:e_commerce_with_backend/utils/device/device_utility.dart';
 import 'package:flutter/material.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
-import '../../../../utils/constants/colors.dart';
+import '../../../../common/widgets/custom_shapes/containers/search_container.dart';
+import '../../../../common/widgets/texts/section_heading.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
-           /// PrimaryHeaderContainer(
-             /// child: Column(
-               /// children: [
+            PrimaryHeaderContainer(
+              child: Column(
+                children: [
                   /// -- AppBar
-                  const HomeAppBar(),
-                  const SizedBox(height: MSizes.spaceBetweenSections,),
+                  HomeAppBar(),
+                  SizedBox(
+                    height: MSizes.spaceBetweenSections,
+                  ),
 
                   /// -- SearchBar
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: MSizes.defaultSpace),
-                    child: Container(
-                      width: MDeviceUtils.getScreenWidth(context),
-                      padding: const EdgeInsets.all(MSizes.md),
-                      decoration: BoxDecoration(
-                        color: Colors.transparent,
-                        borderRadius: BorderRadius.circular(MSizes.cardRadiusLg),
-                        border: Border.all(color: MColors.grey)
-                      ),
-                    ),
-                  )
-
+                  SearchContainer(
+                    text: 'Search in Store',
+                  ),
+                  SizedBox(
+                    height: MSizes.spaceBetweenSections,
+                  ),
 
                   /// -- Categories
+                  Padding(
+                    padding: EdgeInsets.only(left: MSizes.defaultSpace),
+                    child: Column(children: [
+                      SectionHeading(
+                        textHeading: 'Popular Categories',
+                        textColor: Colors.white,
+                      ),
+                    ]),
+                  ),
+                  SizedBox(
+                    height: MSizes.spaceBetweenItems,
+                  ),
+                  HomeCategories()
                 ],
               ),
             )
-    ///      ],
-  ///      ),
-     /// ),
+          ],
+        ),
+      ),
     );
   }
 }
