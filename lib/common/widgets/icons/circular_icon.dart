98836511b2fad487b3eab20a6f5ef2e1a5037853
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-02-06 23:07:28 +0100

    creating circular icon widget

---
+++ lib/common/widgets/icons/circular_icon.dart
@@ -0,0 +1,44 @@
+import 'package:e_commerce_with_backend/utils/constants/sizes.dart';
+import 'package:e_commerce_with_backend/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../../utils/constants/colors.dart';
+
+class CircularIcon extends StatelessWidget {
+  const CircularIcon({
+    super.key,
+    this.width,
+    this.height,
+    this.size = MSizes.lg,
+    required this.icon,
+    this.color,
+    this.backgroundColor,
+    this.onPressed,
+  });
+
+  final double? width, height, size;
+  final IconData icon;
+  final Color? color;
+  final Color? backgroundColor;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+          color: backgroundColor != null
+              ? backgroundColor! : MHelperFunctions.isDarkMode(context) ?
+          MColors.black.withOpacity(0.9) : MColors.white.withOpacity(0.9),
+          borderRadius: BorderRadius.circular(100),
+    ),
+    child: IconButton(
+    onPressed: onPressed,
+    icon: Icon(icon, color: color,size: size),
+    )
+    ,
+    );
+  }
+}
