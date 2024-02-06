9037bf9119b6d1a59147347bbc1d43cfd6a955a3
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-02-06 22:10:47 +0100

    updating image strings with products

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -43,6 +43,54 @@ class MImages{
   static const String promoBanner6="assets/images/banners/newArrival.jpg";
   static const String promoBanner7="assets/images/banners/newKnits.jpg";
 
+  // -- Products
+  static const String productImage1="";
+  static const String productImage2="";
+  static const String productImage3="";
+  static const String productImage4="";
+  static const String productImage5="";
+  static const String productImage6="";
+  static const String productImage7="";
+  static const String productImage8="";
+  static const String productImage9="";
+  static const String productImage10="";
+  static const String productImage11="";
+  static const String productImage12="";
+  static const String productImage13="";
+  static const String productImage14="";
+  static const String productImage15="";
+  static const String productImage16="";
+  static const String productImage17="";
+  static const String productImage18="";
+  static const String productImage19="";
+  static const String productImage20="";
+  static const String productImage21="";
+  static const String productImage22="";
+  static const String productImage23="";
+  static const String productImage24="";
+  static const String productImage25="";
+  static const String productImage26="";
+  static const String productImage27="";
+  static const String productImage28="";
+  static const String productImage29="";
+  static const String productImage30="";
+  static const String productImage31="";
+  static const String productImage32="";
+  static const String productImage33="";
+  static const String productImage34="";
+  static const String productImage35="";
+  static const String productImage36="";
+  static const String productImage37="";
+  static const String productImage38="";
+  static const String productImage39="";
+  static const String productImage40="";
+  static const String productImage41="";
+  static const String productImage42="";
+  static const String productImage43="";
+  static const String productImage44="";
+  static const String productImage45="";
+  static const String productImage46="";
+
 
 
 }
