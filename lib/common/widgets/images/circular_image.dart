16d30fff7620e048cf95bc2e08795968de2baef4
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-02-07 22:44:36 +0100

    creating circular image widget for shop navigation

---
+++ lib/common/widgets/images/circular_image.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import '../../../utils/constants/colors.dart';
+import '../../../utils/constants/image_strings.dart';
+import '../../../utils/constants/sizes.dart';
+import '../../../utils/helpers/helper_functions.dart';
+
+class CircularImage extends StatelessWidget {
+  const CircularImage({
+    super.key,
+    this.fit = BoxFit.cover,
+    required this.image,
+    this.isNetworkImage = false,
+    this.overlayColor,
+    this.backgroundColor,
+    this.height = 56,
+    this.width = 56,
+    this.padding = MSizes.sm,
+  });
+
+  final BoxFit? fit;
+  final String image;
+  final bool isNetworkImage;
+  final Color? overlayColor;
+  final Color? backgroundColor;
+  final double height, width, padding;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      padding: EdgeInsets.all(padding),
+      decoration: BoxDecoration(
+        // if image background color is null then switch it to light and dark mode color design
+        color:
+            MHelperFunctions.isDarkMode(context) ? MColors.dark : MColors.white,
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Center(
+        child: Image(
+          fit: fit,
+          image: isNetworkImage
+              ? NetworkImage(image)
+              : AssetImage(image) as ImageProvider,
+          color: MHelperFunctions.isDarkMode(context)
+              ? MColors.white
+              : MColors.dark,
+        ),
+      ),
+    );
+  }
+}
