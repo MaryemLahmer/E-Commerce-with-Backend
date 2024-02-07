bdaf6efe7f98eac17e68a4290d0d94704ee16241
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-02-07 22:43:31 +0100

    adjusting icon color for dark mode design

--- lib/common/widgets/products/cart/cart_menu_item.dart
+++ lib/common/widgets/products/cart/cart_menu_item.dart
@@ -1,27 +1,26 @@
+import 'package:e_commerce_with_backend/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
-
 import '../../../../utils/constants/colors.dart';
 
 class CartCounterItem extends StatelessWidget {
   const CartCounterItem({
     super.key,
     required this.onPressed,
-    required this.iconColor,
   });
 
   final VoidCallback onPressed;
-  final Color iconColor;
 
   @override
   Widget build(BuildContext context) {
+    final dark = MHelperFunctions.isDarkMode(context);
     return Stack(
       children: [
         IconButton(
             onPressed: onPressed,
             icon: Icon(
               Iconsax.shopping_bag,
-              color: iconColor,
+              color: dark ? MColors.white : MColors.dark,
             )),
         Positioned(
           right: 0,
