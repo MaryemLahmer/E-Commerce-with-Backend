e40d2f45d90bf83f055c4f029d3844dc213a52cc
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-02-07 23:50:53 +0100

    updating store screen with gid layout for brand section

--- lib/features/shop/screens/store/store.dart
+++ lib/features/shop/screens/store/store.dart
@@ -1,14 +1,17 @@
 import 'package:e_commerce_with_backend/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:e_commerce_with_backend/common/widgets/custom_shapes/containers/search_container.dart';
+import 'package:e_commerce_with_backend/common/widgets/layouts/grid_layout.dart';
 import 'package:e_commerce_with_backend/common/widgets/products/cart/cart_menu_item.dart';
 import 'package:e_commerce_with_backend/common/widgets/texts/section_heading.dart';
 import 'package:e_commerce_with_backend/utils/constants/colors.dart';
+import 'package:e_commerce_with_backend/utils/constants/enums.dart';
 import 'package:e_commerce_with_backend/utils/constants/image_strings.dart';
 import 'package:e_commerce_with_backend/utils/constants/sizes.dart';
 import 'package:e_commerce_with_backend/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 import '../../../../common/widgets/appbar/appbar.dart';
 import '../../../../common/widgets/images/circular_image.dart';
+import '../../../../common/widgets/texts/brand_title_text_with_verified_icon.dart';
 
 class StoreScreen extends StatelessWidget {
   const StoreScreen({super.key});
@@ -19,7 +22,10 @@ class StoreScreen extends StatelessWidget {
       appBar: MAppBar(
         title: Text(
           'Store',
-          style: Theme.of(context).textTheme.headlineLarge,
+          style: Theme
+              .of(context)
+              .textTheme
+              .headlineLarge,
         ),
         actions: [
           CartCounterItem(
@@ -44,6 +50,7 @@ class StoreScreen extends StatelessWidget {
                   shrinkWrap: true,
                   physics: const NeverScrollableScrollPhysics(),
                   children: [
+
                     /// Search Bar
                     const SizedBox(
                       height: MSizes.spaceBetweenItems,
@@ -67,17 +74,57 @@ class StoreScreen extends StatelessWidget {
                       height: MSizes.spaceBetweenItems / 1.5,
                     ),
 
-                    const RoundedContainer(
-                      padding: EdgeInsets.all(MSizes.sm),
-                      showBorder: true,
-                      backgroundColor: Colors.transparent,
-                      child: Row(
-                        children: [
-                          /// Icon
-                          CircularImage(image: MImages.clothIcon,)
-                        ],
-                      ),
-                    )
+                    MGridLayout(itemCount: 4,mainAxisExtent: 80, itemBuilder: (_, index) {
+                      return GestureDetector(
+                        onTap: () {},
+                        child: RoundedContainer(
+                          padding: const EdgeInsets.all(MSizes.xs),
+                          showBorder: true,
+                          backgroundColor: Colors.transparent,
+                          child: Row(
+                            children: [
+
+                              /// Icon
+                              Flexible( /// flexible only takes the needed space for the child
+                                child: CircularImage(
+                                  image: MImages.clothIcon,
+                                  overlayColor: MHelperFunctions.isDarkMode(
+                                      context)
+                                      ? MColors.white
+                                      : MColors.black,
+                                  backgroundColor: Colors.transparent,
+                                ),
+                              ),
+                              const SizedBox(
+                                width: MSizes.spaceBetweenItems / 2,
+                              ),
+
+                              /// Text
+                              Expanded( /// expanded takes up the maximum space given
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const BrandTitleWithVerifiedIcon(
+                                      title: 'Nike',
+                                      brandTextSizes: TextSizes.large,
+                                    ),
+                                    Text(
+                                      '256 products',
+                                      overflow: TextOverflow.ellipsis,
+                                      style: Theme
+                                          .of(context)
+                                          .textTheme
+                                          .labelMedium,
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    })
                   ],
                 ),
               ),
@@ -89,4 +136,3 @@ class StoreScreen extends StatelessWidget {
     );
   }
 }
-
