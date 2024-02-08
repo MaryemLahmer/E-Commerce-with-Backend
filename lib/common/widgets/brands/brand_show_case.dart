d1f0028a1a0e501e2c2adbbd044a1234bd5b02e4
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-02-08 16:25:47 +0100

    adjusting

--- lib/common/widgets/brands/brand_show_case.dart
+++ lib/common/widgets/brands/brand_show_case.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import '../../../utils/constants/colors.dart';
-import '../../../utils/constants/image_strings.dart';
 import '../../../utils/constants/sizes.dart';
 import '../../../utils/helpers/helper_functions.dart';
 import '../custom_shapes/containers/rounded_container.dart';
 import 'brand_card.dart';
 
-
 class BrandShowCse extends StatelessWidget {
   const BrandShowCse({
-    super.key, required this.images,
+    super.key,
+    required this.images,
   });
 
   final List<String> images;
@@ -24,14 +23,17 @@ class BrandShowCse extends StatelessWidget {
       margin: const EdgeInsets.only(bottom: MSizes.spaceBetweenItems),
       child: Column(
         children: [
-
           /// Brand with Product Count
           const BrandCard(showBorder: false),
+          const SizedBox(
+            height: MSizes.spaceBetweenItems,
+          ),
 
           /// Brand top 3 Product Images
           Row(
-            children: images.map((image) =>
-                brandTopProductImageWidget(image, context)).toList(),
+            children: images
+                .map((image) => brandTopProductImageWidget(image, context))
+                .toList(),
           )
         ],
       ),
@@ -41,13 +43,15 @@ class BrandShowCse extends StatelessWidget {
   Widget brandTopProductImageWidget(String image, context) {
     return Expanded(
         child: RoundedContainer(
-          height: 90,
-          padding: const EdgeInsets.all(MSizes.xs),
-          margin: const EdgeInsets.only(right: MSizes.sm),
-          backgroundColor: MHelperFunctions.isDarkMode(context) ? MColors
-              .darkerGrey : MColors.light,
-          child: Image(image: AssetImage(image),),
-        )
-    );
+      height: 90,
+      padding: const EdgeInsets.all(MSizes.xs),
+      margin: const EdgeInsets.only(right: MSizes.sm),
+      backgroundColor: MHelperFunctions.isDarkMode(context)
+          ? MColors.darkerGrey
+          : MColors.light,
+      child: Image(
+        image: AssetImage(image),
+      ),
+    ));
   }
 }
