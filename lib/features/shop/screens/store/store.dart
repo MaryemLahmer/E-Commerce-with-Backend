f45579690fd00138345b5e4c109916d889c63206
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-02-08 13:42:23 +0100

    adding tab bar with tabs to the store screen

--- lib/features/shop/screens/store/store.dart
+++ lib/features/shop/screens/store/store.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_with_backend/common/widgets/appbar/tabbar.dart';
 import 'package:e_commerce_with_backend/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:e_commerce_with_backend/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:e_commerce_with_backend/common/widgets/layouts/grid_layout.dart';
@@ -18,120 +19,134 @@ class StoreScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: MAppBar(
-        title: Text(
-          'Store',
-          style: Theme
-              .of(context)
-              .textTheme
-              .headlineLarge,
-        ),
-        actions: [
-          CartCounterItem(
-            onPressed: () {},
-          )
-        ],
-      ),
-      body: NestedScrollView(
-        headerSliverBuilder: (_, innerBoxIsScrolled) {
-          return [
-            SliverAppBar(
-              automaticallyImplyLeading: false,
-              pinned: true,
-              floating: true,
-              backgroundColor: MHelperFunctions.isDarkMode(context)
-                  ? MColors.black
-                  : MColors.white,
-              expandedHeight: 440,
-              flexibleSpace: Padding(
-                padding: const EdgeInsets.all(MSizes.defaultSpace),
-                child: ListView(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  children: [
-
-                    /// Search Bar
-                    const SizedBox(
-                      height: MSizes.spaceBetweenItems,
-                    ),
-                    const SearchContainer(
-                      text: 'Search in Store',
-                      showBorder: true,
-                      showBackground: false,
-                      padding: EdgeInsets.zero,
-                    ),
-                    const SizedBox(
-                      height: MSizes.spaceBetweenSections,
-                    ),
-
-                    /// Featured Brands
-                    SectionHeading(
-                      textHeading: 'Featured Brands',
-                      onPressed: () {},
-                    ),
-                    const SizedBox(
-                      height: MSizes.spaceBetweenItems / 1.5,
-                    ),
-
-                    MGridLayout(itemCount: 4,mainAxisExtent: 80, itemBuilder: (_, index) {
-                      return GestureDetector(
-                        onTap: () {},
-                        child: RoundedContainer(
-                          padding: const EdgeInsets.all(MSizes.xs),
-                          showBorder: true,
-                          backgroundColor: Colors.transparent,
-                          child: Row(
-                            children: [
-
-                              /// Icon
-                              Flexible( /// flexible only takes the needed space for the child
-                                child: CircularImage(
-                                  image: MImages.clothIcon,
-                                  overlayColor: MHelperFunctions.isDarkMode(
-                                      context)
-                                      ? MColors.white
-                                      : MColors.black,
-                                  backgroundColor: Colors.transparent,
-                                ),
-                              ),
-                              const SizedBox(
-                                width: MSizes.spaceBetweenItems / 2,
-                              ),
-
-                              /// Text
-                              Expanded( /// expanded takes up the maximum space given
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    const BrandTitleWithVerifiedIcon(
-                                      title: 'Nike',
-                                      brandTextSizes: TextSizes.large,
-                                    ),
-                                    Text(
-                                      '256 products',
-                                      overflow: TextOverflow.ellipsis,
-                                      style: Theme
-                                          .of(context)
-                                          .textTheme
-                                          .labelMedium,
-                                    )
-                                  ],
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      );
-                    })
-                  ],
-                ),
-              ),
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: MAppBar(
+          title: Text(
+            'Store',
+            style: Theme.of(context).textTheme.headlineLarge,
+          ),
+          actions: [
+            CartCounterItem(
+              onPressed: () {},
             )
-          ];
-        },
-        body: Container(),
+          ],
+        ),
+        body: NestedScrollView(
+          headerSliverBuilder: (_, innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                  automaticallyImplyLeading: false,
+                  pinned: true,
+                  floating: true,
+                  backgroundColor: MHelperFunctions.isDarkMode(context)
+                      ? MColors.black
+                      : MColors.white,
+                  expandedHeight: 440,
+                  flexibleSpace: Padding(
+                    padding: const EdgeInsets.all(MSizes.defaultSpace),
+                    child: ListView(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      children: [
+                        /// Search Bar
+                        const SizedBox(
+                          height: MSizes.spaceBetweenItems,
+                        ),
+                        const SearchContainer(
+                          text: 'Search in Store',
+                          showBorder: true,
+                          showBackground: false,
+                          padding: EdgeInsets.zero,
+                        ),
+                        const SizedBox(
+                          height: MSizes.spaceBetweenSections,
+                        ),
+
+                        /// Featured Brands
+                        SectionHeading(
+                          textHeading: 'Featured Brands',
+                          onPressed: () {},
+                        ),
+                        const SizedBox(
+                          height: MSizes.spaceBetweenItems / 1.5,
+                        ),
+
+                        MGridLayout(
+                            itemCount: 4,
+                            mainAxisExtent: 80,
+                            itemBuilder: (_, index) {
+                              return GestureDetector(
+                                onTap: () {},
+                                child: RoundedContainer(
+                                  padding: const EdgeInsets.all(MSizes.xs),
+                                  showBorder: false,
+                                  backgroundColor: Colors.transparent,
+                                  child: Row(
+                                    children: [
+                                      /// Icon
+                                      Flexible(
+                                        /// flexible only takes the needed space for the child
+                                        child: CircularImage(
+                                          image: MImages.clothIcon,
+                                          overlayColor:
+                                              MHelperFunctions.isDarkMode(
+                                                      context)
+                                                  ? MColors.white
+                                                  : MColors.black,
+                                          backgroundColor: Colors.transparent,
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        width: MSizes.spaceBetweenItems / 2,
+                                      ),
+
+                                      /// Text
+                                      Expanded(
+                                        /// expanded takes up the maximum space given
+                                        child: Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            const BrandTitleWithVerifiedIcon(
+                                              title: 'Nike',
+                                              brandTextSizes: TextSizes.large,
+                                            ),
+                                            Text(
+                                              '256 products',
+                                              overflow: TextOverflow.ellipsis,
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .labelMedium,
+                                            )
+                                          ],
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              );
+                            })
+                      ],
+                    ),
+                  ),
+
+                  /// Tabs
+                  bottom: const MTabBar(tabs: [
+                    Tab(
+                      child: Text('Sports'),
+                    ),
+                    Tab(child: Text('Furniture'),),
+                    Tab(child: Text('Electronics'),),
+                    Tab(child: Text('Clothes'),),
+                    Tab(child: Text('Cosmetics'),),
+                  ]))
+            ];
+          },
+          body: ,
+        ),
       ),
     );
   }
