b70a4edeed1953c3b11f843abcded04400b424d7
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-02-08 17:25:59 +0100

    adjusting colors

--- lib/common/widgets/images/image_text.dart
+++ lib/common/widgets/images/image_text.dart
@@ -8,7 +8,7 @@ class VerticalImageText extends StatelessWidget {
     required this.image,
     required this.title,
     this.textColor = MColors.white,
-    this.backgroundColor = MColors.white,
+    this.backgroundColor,
     this.onTap,
   });
 
