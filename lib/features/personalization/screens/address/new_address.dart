159d2d98b9672a1a41cf1fef25750c9ebfe071ae
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-02-12 21:12:15 +0100

    new address screen

---
+++ lib/features/personalization/screens/address/new_address.dart
@@ -0,0 +1,76 @@
+import 'package:e_commerce_with_backend/common/widgets/appbar/appbar.dart';
+import 'package:e_commerce_with_backend/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class AddNewAddressScreen extends StatelessWidget {
+  const AddNewAddressScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const MAppBar(
+        showBackArrow: true,
+        title: Text('Add New Address'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(MSizes.defaultSpace),
+          child: Form(
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(Iconsax.user), labelText: 'Name'),
+                ),
+                const SizedBox(height: MSizes.spaceBtwInputFields,),
+                TextFormField(
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(Iconsax.mobile), labelText: 'Phone Number'),
+                ),
+                const SizedBox(height: MSizes.spaceBtwInputFields,),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            prefixIcon: Icon(Iconsax.building_31), labelText: 'Street'),
+                      ),
+                    ),
+                    const SizedBox(width: MSizes.spaceBtwInputFields/2,),
+                    Expanded(
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            prefixIcon: Icon(Iconsax.code), labelText: 'Postal Code'),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: MSizes.spaceBtwInputFields,),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            prefixIcon: Icon(Iconsax.building_31), labelText: 'Street'),
+                      ),
+                    ),
+                    const SizedBox(width: MSizes.spaceBtwInputFields/2,),
+                    Expanded(
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            prefixIcon: Icon(Iconsax.code), labelText: 'Postal Code'),
+                      ),
+                    ),
+                  ],
+                ),
+
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
