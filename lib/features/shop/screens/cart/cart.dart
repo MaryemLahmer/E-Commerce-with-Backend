b75ea6a7ef03864ceb4dabed79f8efde9ee1e410
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-02-13 23:15:03 +0100

    adding cart items

--- lib/features/shop/screens/cart/cart.dart
+++ lib/features/shop/screens/cart/cart.dart
@@ -1,10 +1,13 @@
 import 'package:e_commerce_with_backend/common/widgets/appbar/appbar.dart';
 import 'package:e_commerce_with_backend/common/widgets/images/rounded_images.dart';
+import 'package:e_commerce_with_backend/common/widgets/texts/brand_title_text_with_verified_icon.dart';
+import 'package:e_commerce_with_backend/common/widgets/texts/product_title_text.dart';
 import 'package:e_commerce_with_backend/utils/constants/colors.dart';
 import 'package:e_commerce_with_backend/utils/constants/sizes.dart';
 import 'package:e_commerce_with_backend/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../common/widgets/products/cart/cart_item.dart';
 import '../../../../utils/constants/image_strings.dart';
 
 class CartScreen extends StatelessWidget {
@@ -29,20 +32,9 @@ class CartScreen extends StatelessWidget {
               separatorBuilder: (_, __) => const SizedBox(
                 height: MSizes.spaceBetweenSections,
               ),
-              itemBuilder: (_, index) => Column(
+              itemBuilder: (_, index) => const Column(
                 children: [
-                  Row(
-                    children: [
-                      RoundedImage(
-                        imageUrl: MImages.productImage5,
-                        height: 60,
-                        width: 60,
-                        padding: const EdgeInsets.all(MSizes.sm),
-                        backgroundColor: MHelperFunctions.isDarkMode(context) ? MColors.darkerGrey:MColors.light,
-                      ),
-                      const SizedBox(width: MSizes.spaceBetweenItems,)
-                    ],
-                  )
+                  CartItem()
                 ],
               ),
             )),
@@ -50,3 +42,4 @@ class CartScreen extends StatelessWidget {
     );
   }
 }
+
