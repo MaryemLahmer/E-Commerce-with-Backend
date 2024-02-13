9944f236c27830338772fc7443b221f0411a0a15
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-02-13 23:45:33 +0100

    adding add/remove button

--- lib/features/shop/screens/cart/cart.dart
+++ lib/features/shop/screens/cart/cart.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce_with_backend/common/widgets/appbar/appbar.dart';
+import 'package:e_commerce_with_backend/common/widgets/icons/circular_icon.dart';
 import 'package:e_commerce_with_backend/common/widgets/images/rounded_images.dart';
 import 'package:e_commerce_with_backend/common/widgets/texts/brand_title_text_with_verified_icon.dart';
 import 'package:e_commerce_with_backend/common/widgets/texts/product_title_text.dart';
@@ -6,7 +7,9 @@ import 'package:e_commerce_with_backend/utils/constants/colors.dart';
 import 'package:e_commerce_with_backend/utils/constants/sizes.dart';
 import 'package:e_commerce_with_backend/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
+import '../../../../common/widgets/products/cart/add_remove_button.dart';
 import '../../../../common/widgets/products/cart/cart_item.dart';
 import '../../../../utils/constants/image_strings.dart';
 
@@ -32,9 +35,34 @@ class CartScreen extends StatelessWidget {
               separatorBuilder: (_, __) => const SizedBox(
                 height: MSizes.spaceBetweenSections,
               ),
-              itemBuilder: (_, index) => const Column(
+              itemBuilder: (_, index) =>  Column(
                 children: [
-                  CartItem()
+                  const CartItem(),
+                  const SizedBox(
+                    height: MSizes.spaceBetweenItems,
+                  ),
+                  Row(
+                    children: [
+                      const SizedBox(width: 70,),
+                      /// Add Remove Buttons
+                      const ProductQuantityWithAddRemove(),
+                      const SizedBox(
+                        width: MSizes.spaceBetweenItems,
+                      ),
+                      Text('2',style: Theme.of(context).textTheme.titleSmall),
+                      const SizedBox(
+                        width: MSizes.spaceBetweenItems,
+                      ),
+                      CircularIcon(
+                        icon: Iconsax.add,
+                        width: 32,
+                        height: 32,
+                        size: MSizes.md,
+                        color: MColors.white ,
+                        backgroundColor: MColors.primary.withOpacity(1),
+                      ),
+                    ],
+                  )
                 ],
               ),
             )),
