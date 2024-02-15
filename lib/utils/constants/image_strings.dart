0b5601b261733a9b4b21d33afc78a9bbf8602789
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-02-15 15:44:16 +0100

    adding payment assets strings

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -49,6 +49,15 @@ class MImages{
   static const String user3="assets/images/reviews/user3.png";
   static const String user4="assets/images/reviews/user4.png";
 
+  // Payment Methods
+  static const String applePay="assets/icons/payment_methods/apple_pay.png";
+  static const String googleWallet="assets/icons/payment_methods/google_wallet.png";
+  static const String creditCard="assets/icons/payment_methods/credit_card.png";
+  static const String masterCard="assets/icons/payment_methods/master_card.png";
+  static const String paypal ="assets/icons/payment_methods/paypal.png";
+  static const String paytm ="assets/icons/payment_methods/paytm.png";
+  static const String successPayment ="assets/icons/payment_methods/successful_payment.png";
+  static const String visa ="assets/icons/payment_methods/visa.png";
 
   // -- User Profile
   static const String user="assets/images/user/user.png";
