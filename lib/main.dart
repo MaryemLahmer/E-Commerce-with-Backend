e26dfd54b0dce289be8e1dd72c0c3405aff5bb32
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-02-24 12:56:54 +0100

    adding firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
-
 import 'App.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'firebase_options.dart';
+import 'package:get/get.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   /// Tod: Add Widgets Binding
   /// Todo: Init local Storage
+  /// Todo: Init payment methods
   /// Todo: Await Native Splash
   /// Todo: Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ).then((FirebaseApp value) => Get.put(AuthenticationRepository()),);
+
   /// Todo: Initialize Authentication
   runApp(const App());
 }
 
-class FlutterNativeSplash {
-}
-
+class FlutterNativeSplash {}
