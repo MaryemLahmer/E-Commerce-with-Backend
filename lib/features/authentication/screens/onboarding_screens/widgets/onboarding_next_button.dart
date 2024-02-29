7b3d3498847b9dd220c6115d8e0472a192ced189
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-02-29 14:06:31 +0100

    adjusting path

--- lib/features/authentication/screens/onboarding_screens/widgets/onboarding_next_button.dart
+++ lib/features/authentication/screens/onboarding_screens/widgets/onboarding_next_button.dart
@@ -1,10 +1,10 @@
-import 'package:e_commerce_with_backend/features/authentication/controllers.onboarding/onboarding_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 import '../../../../../utils/constants/colors.dart';
 import '../../../../../utils/constants/sizes.dart';
 import '../../../../../utils/device/device_utility.dart';
 import '../../../../../utils/helpers/helper_functions.dart';
+import '../../../controllers/onboarding/onboarding_controller.dart';
 
 class OnBoardingNextBottom extends StatelessWidget {
   const OnBoardingNextBottom({
