6b35dfa4d5b6ae8bf1adc186ae636b0d82c8c020
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-02-29 13:41:32 +0100

    binding elements with splash screen and authen repo

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 import 'App.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'data/repositories/authentication_repo.dart';
 import 'firebase_options.dart';
 import 'firebase_options.dart';
 import 'package:get/get.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  /// Widgets Binding
+  final WidgetsBinding widgetBinding = WidgetsFlutterBinding.ensureInitialized();
 
-  /// Tod: Add Widgets Binding
-  /// Todo: Init local Storage
+  /// Init local Storage
+  await GetStorage.init();
+
   /// Todo: Init payment methods
   /// Todo: Await Native Splash
-  /// Todo: Initialize Firebase
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+  FlutterNativeSplash.preserve(widgetsBinding: widgetBinding);
+  /// Todo: Initialize Firebase & Authentication Repository
+ await Firebase.initializeApp(
+   options: DefaultFirebaseOptions.currentPlatform,
   ).then((FirebaseApp value) => Get.put(AuthenticationRepository()),);
 
   /// Todo: Initialize Authentication
   runApp(const App());
 }
 
-class FlutterNativeSplash {}
